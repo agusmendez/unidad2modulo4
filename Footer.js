041c1c06a4b25c81709eb5887491849d7cc7ddd5
Agustin Mendez <89879707+agusmendez@users.noreply.github.com>
2022-01-06 21:16:48 -0300

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,9 @@
+import '../../estilos/components/layout/Footer.css'
+
+import React from 'react';
+const Footer = (props) => {
+    return (
+        <footer>Diseñado por Agustín Mendez - &copy; 2021</footer>
+    );
+}
+export default Footer;
